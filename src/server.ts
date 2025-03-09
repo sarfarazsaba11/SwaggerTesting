e4a566a24460750cacd8733869f48f336909d901
Saba Sarfaraz <sarfarazsaba11@gmail.com>
2025-03-09 22:48:05 +0500

    Update script

--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,14 @@ setupSwagger(app);
 app.use("/api", router);
 
 // Export the handler for Vercel
+const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
 export default app;
+
+
